7132575630dbc27c66d2444de3b1dea63d18886a
Megalomaniac Schtkan <texview@gmx.com>
2024-11-15 20:21:52 +0300

    Export spreedsheet function to drivers

---
+++ Services/DeviceTunerNET.Services.Interfaces/ITablesManager.cs
@@ -0,0 +1,22 @@
+﻿namespace DeviceTunerNET.Services.Interfaces
+{
+    public interface ITablesManager
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        bool SetCurrentDocument(string document);
+        bool SetCurrentPageByName(string pageName);
+        bool CreateNewPage(string pageName);
+
+        int GetLastRowIndex();
+        int GetLastColumnIndex();
+
+        string GetCellValueByIndex(int row, int column);
+        void SetCellValueByIndex(string cellValue, int row, int column);
+
+        void SetCellColor(string cellColor, System.Drawing.Color color, int row, int column);
+
+        void Save();
+    }
+}
---
+++ Services/DeviceTunerNET.Services/ExcelDriver.cs
@@ -0,0 +1,100 @@
+﻿using DeviceTunerNET.Services.Interfaces;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceTunerNET.Services
+{
+    public class ExcelDriver : ITablesManager
+    {
+        private FileInfo sourceFile;
+        private ExcelPackage package;
+        private ExcelWorksheet worksheet;
+
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public ExcelDriver()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        public bool SetCurrentDocument(string document)
+        {
+            try
+            {
+                sourceFile = new FileInfo(document);
+                package = new ExcelPackage(sourceFile);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+            if(!SetCurrentPageByName("Адреса"))
+            { 
+                return false; 
+            }
+
+            // get number of rows and columns in the sheet
+            Rows = worksheet.Dimension.Rows; // 20
+            Columns = worksheet.Dimension.Columns; // 7
+            return true;
+        }
+
+        public bool CreateNewPage(string pageName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCellValueByIndex(int row, int column)
+        {
+            return worksheet.Cells[row, column].Value?.ToString();
+        }
+
+        public int GetLastColumnIndex()
+        {
+            return worksheet.Dimension.Columns;
+        }
+
+        public int GetLastRowIndex()
+        {
+            return worksheet.Dimension.Rows;
+        }
+
+        public void Save()
+        {
+            package.Save();
+        }
+
+        public void SetCellColor(string cellColor, System.Drawing.Color color, int row, int column)
+        {
+            worksheet.Cells[row, column].Style.Font.Color.SetColor(color);
+        }
+
+        public void SetCellValueByIndex(string cellValue, int row, int column)
+        {
+            worksheet.Cells[row, column].Value = cellValue;
+        }
+
+        public bool SetCurrentPageByName(string pageName)
+        {
+            try
+            {
+                worksheet = package.Workbook.Worksheets[pageName];
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
