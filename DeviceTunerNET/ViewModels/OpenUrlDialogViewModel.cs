c0977ef1c3eab78bdb09e705b89c5fbea9a8092f
Megalomaniac Schtkan <texview@gmx.com>
2024-11-17 01:26:17 +0300

    Repair logic and view

--- DeviceTunerNET/ViewModels/OpenUrlDialogViewModel.cs
+++ DeviceTunerNET/ViewModels/OpenUrlDialogViewModel.cs
@@ -13,15 +13,42 @@ namespace DeviceTunerNET.ViewModels
 {
     internal class OpenUrlDialogViewModel : BindableBase, IDialogAware
     {
-        private DelegateCommand<string> _closeDialogCommand;
-        public DelegateCommand<string> CloseDialogCommand =>
-            _closeDialogCommand ??= new DelegateCommand<string>(CloseDialog);
+        private DelegateCommand<string> _okDialogCommand;
+        public DelegateCommand<string> OkDialogCommand =>
+            _okDialogCommand ??= new DelegateCommand<string>(CloseDialog, CanExecuteCloseDialog)
+                .ObservesProperty(() => SelectedSheet);
 
-        private ObservableCollection<UrlItem> _historyUrls;
-        public ObservableCollection<UrlItem> HistoryUrls { get => _historyUrls; set => SetProperty(ref _historyUrls, value); }
+        private bool CanExecuteCloseDialog(string arg)
+        {
+            return SelectedSheet != null;
+        }
 
-        private UrlItem selectedUrl;
-        public UrlItem SelectedUrl { get => selectedUrl; set => SetProperty(ref selectedUrl, value); }
+        private ObservableCollection<UrlItem> _filteredSheets = [];
+        public ObservableCollection<UrlItem> FilteredSheets
+        {
+            get => _filteredSheets;
+            set => SetProperty(ref _filteredSheets, value);
+        }
+
+        private readonly List<UrlItem> _availableSpreadsheets = [];
+
+        private UrlItem _selectedSheet;
+        public UrlItem SelectedSheet 
+        { 
+            get => _selectedSheet; 
+            set => SetProperty(ref _selectedSheet, value); 
+        }
+
+        private string _searchTextbox = string.Empty;
+        public string SearchTextbox
+        {
+            get => _searchTextbox;
+            set
+            {
+                SetProperty(ref _searchTextbox, value);
+                FilterSpreadsheets();
+            }
+        }
 
         public DialogCloseListener RequestClose { get; }
 
@@ -40,8 +67,8 @@ namespace DeviceTunerNET.ViewModels
                 result = ButtonResult.Cancel;
             var parameters = new DialogParameters()
             {
-                {"urlName", SelectedUrl.Name},
-                {"urlPath", SelectedUrl.Url},
+                {"urlName", SelectedSheet.Name},
+                {"urlPath", SelectedSheet.Url},
             };
             var dialogResult = new DialogResult()
             {
@@ -60,11 +87,34 @@ namespace DeviceTunerNET.ViewModels
         public void OnDialogOpened(IDialogParameters parameters)
         {
             var urls = parameters.GetValue<IEnumerable<UrlItem>>("historyUrls");
-            HistoryUrls = [];
             foreach (var url in urls)
-            { 
-                HistoryUrls.Add(url);
+            {
+                _availableSpreadsheets.Add(url);
+                FilteredSheets.Add(url);
             }
         }
+
+        private void FilterSpreadsheets()
+        {
+            FilteredSheets.Clear();
+            if (string.IsNullOrEmpty(_searchTextbox))
+            { 
+                foreach (var spreadsheet in _availableSpreadsheets)
+                {
+                    FilteredSheets.Add(spreadsheet);
+                }
+                return;
+            } 
+            foreach (var spreadsheet in _availableSpreadsheets)
+            {
+                if (spreadsheet == null)
+                    return;
+                if(spreadsheet.Name.Contains(_searchTextbox, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    FilteredSheets.Add(spreadsheet);
+                }
+            }
+        }
+
     }
 }
